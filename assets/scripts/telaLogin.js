109ce896db71ff1c9c357160e668de15d0e9216d
analacaze <anaclaralacaze@gmail.com>
2020-09-22 16:49:01 -0300

    Tela de Login - request para servidor

--- assets/scripts/telaLogin.js
+++ assets/scripts/telaLogin.js
@@ -6,5 +6,9 @@ senha.value="";
 function entrarLogin(){
     if (email.value==="" || senha.value===""){
         alert("Preencha os campos email e senha");
+    }else{
+        var login = {email: email.value, password: senha.value}
+        var requisicao = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v1/buzzquizz/users",login);
     }
 }
+
