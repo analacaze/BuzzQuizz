632910a5e1fc407c9a18498158f1ec1b71adcee1
analacaze <anaclaralacaze@gmail.com>
2020-09-22 18:42:16 -0300

    Tela de Login - botao desabilitado

--- assets/scripts/telaLogin.js
+++ assets/scripts/telaLogin.js
@@ -1,5 +1,6 @@
 var email = document.querySelector(".tela-login .email");
 var senha = document.querySelector(".tela-login .senha");
+var botao = document.querySelector(".tela-login button");
 email.value="";
 senha.value="";
 
@@ -7,14 +8,20 @@ function entrarLogin(){
     if (email.value==="" || senha.value===""){
         alert("Preencha os campos email e senha");
     }else{
+        desabilitarBotao();
         var login = {email: email.value, password: senha.value}
         var requisicao = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v1/buzzquizz/users",login);
-    }
-    requisicao.then(abrirTelaQuizzes).catch(erroLogin);
+        requisicao.then(abrirTelaQuizzes).catch(erroLogin);
+    }    
 }
-
+function desabilitarBotao(){
+    botao.disabled = true;
+}
+function habilitarBotao(){
+    botao.disabled = false;
+}
 function erroLogin(erro){
     alert("Email/Senha incorretos");
-    console.log(erro);
+    habilitarBotao();
 }
 
