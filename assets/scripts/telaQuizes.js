5494539936bbeac2af46b6815722c62bfcbec5b1
analacaze <anaclaralacaze@gmail.com>
2020-09-25 18:17:47 -0300

    Bonus - deletar quiz

--- assets/scripts/telaQuizes.js
+++ assets/scripts/telaQuizes.js
@@ -12,7 +12,9 @@ function trocarTelas(seletorTelaAntiga,seletorTelaAtual){
     telaAtual.classList.remove("fechada");
 }
 function salvarToken(token){
-    headerToken.headers["User-Token"] = token;
+    if (token !== undefined){
+        headerToken.headers["User-Token"] = token;
+    }    
 }
 function pegarQuizes(){
     var requisicao = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v1/buzzquizz/quizzes",headerToken);
@@ -34,6 +36,15 @@ function criarNovoQuiz(){
 function abrirQuiz(quiz){
     abrirTelaQuiz(quiz);
 }
+function deletarQuiz(id){
+    var requisicao = axios.delete("https://mock-api.bootcamp.respondeai.com.br/api/v1/buzzquizz/quizzes/"+id,headerToken);
+    requisicao.then(recarregarQuizes);
+}
+function recarregarQuizes(){
+    trocarTelas(".tela-quiz",".tela-quizes");
+    salvarToken(undefined);
+    pegarQuizes();
+}
 
 //Renderizar
 function resetarQuizes(quizes){
@@ -41,6 +52,7 @@ function resetarQuizes(quizes){
 }
 function renderizarQuizes(quiz,data){
     quiz.innerText = data.title;
+    quiz.innerHTML += "<ion-icon onclick='deletarQuiz("+data.id+")' class='icone deletar' name='trash'></ion-icon>";
     quiz.setAttribute("class","quiz");
     quiz.setAttribute("id",data.id);
     quiz.setAttribute("onclick","abrirQuiz(this)");
